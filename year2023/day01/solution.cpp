013d45b87aead7b05e041a79ee167496ac074210
Aditya Parmar <adityaparmar@Adityas-MacBook-Pro-3.local>
2023-11-30 23:29:57 -0800

    fix p2

--- year2023/day01/solution.cpp
+++ year2023/day01/solution.cpp
@@ -24,6 +24,29 @@ ostream &operator<<(ostream &os, const vector<S> &vector) {
   return os;
 }
 
+vector<int> substring1(string s, string sub_s) {
+
+  vector<int> indices;
+
+  // return the start indices of each occurrence of sub_s in s
+  for (int i; i < s.length(); i++) {
+
+    if (s[i] != sub_s[0]) {
+      continue;
+    }
+    bool found_occurrence = false;
+    for (int j = 1; j < sub_s.length(); j++) {
+      if (s[i+j] == sub_s[j]) {
+        found_occurrence = true;
+        }
+    }
+    if (found_occurrence) {
+      indices.push_back(i);
+    }
+  }
+  return indices;
+}
+
 void part1(vector<string> input) {
   int result = 0;
   for (string s : input) {
@@ -44,8 +67,30 @@ void part1(vector<string> input) {
 
 void part2(vector<string> input) {
   int result = 0;
+  for (string s : input) {
+    int first = -1, last = -1;
+
+    vector<string> digit_words = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    vector<string> digits = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    
+    for (int i = 0; i < 9; i++) {
+      vector<int> digit_word_indices =  substring1(s, digit_words[i]);
+      vector<int> digits_indices =  substring1(s, digits[i]);
+
+      int minimum = min(digit_word_indices[0], digits_indices[0]);
+      int maximum = max(digit_word_indices[0], digits_indices[0]);
+
+      first = min(minimum, first);
+      last = max(maximum, last);
+    }
+
+    int num = first * 10 + last;
+    result += num;
+  }
   cout << "Part 2: " << result << endl;
 }
+
+
 
 vector<string> read_input() {
   ifstream input_file;
